4f843f276d6cba52b95d052c80eb15569511f2d2
Shivam Kushwaha <proguideshivam@gmail.com>
2022-10-30 19:13:47 +0530

    added Static CSS

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
 
+
+posts = [
+	{
+		'authors' : 'Shivam Kushwaha',
+		'Title' : 'This is first prost.',
+		'Descreption' : 'This IS frist post descreption.',
+		'post_date' : '2 Aug 2023',
+	},
+	{
+		'authors' : 'Santosh Kushwaha',
+		'Title' : 'This is Second prost.',
+		'Descreption' : 'This IS Second post descreption.',
+		'post_date' : '2 Sep 2023',
+	},
+	{
+		'authors' : 'Aman Kushwaha',
+		'Title' : 'This is Third prost.',
+		'Descreption' : 'This IS Third post descreption.',
+		'post_date' : '2 Aug 2023',
+	}
+]
 def home(request):
-	return render(request, 'blog/home.html')
+	context = {
+		'posts' :posts
+	}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return render(request, 'blog/about.html')
+	return render(request, 'blog/about.html', {'title': 'about'})
